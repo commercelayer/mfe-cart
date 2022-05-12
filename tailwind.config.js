6b96bcce27377df3b6882749fb8a014860afe9b5
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2022-05-12 11:46:10 +0200

    chore: add basic next config and tailwind config from shared lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
+const config = require("@commercelayer/react-utils").tailwindConfig
+
 module.exports = {
-  content: [
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    fontFamily: {
-      sans: ["Manrope", "ui-sans-serif", "sans-serif"],
-    },
-    extend: {},
-  },
-  plugins: [],
+  ...config
 }
