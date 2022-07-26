62d6e0e3fceeaeae7dd6aa73b4380084db79e892
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2022-07-26 12:49:12 +0200

    fix: add slug as env to enforce security when app is not running in cl-hosted mode

--- utils/isValidHost.ts
+++ utils/isValidHost.ts
@@ -7,22 +7,28 @@ export const makeSubdomain = (hostname: string) => {
 const isProduction = (forceProductionEnv?: boolean) =>
   forceProductionEnv ? true : process.env.NODE_ENV === "production"
 
-const isHosted = () => !!process.env.NEXT_PUBLIC_HOSTED
+const isCommerceLayerHosted = () => Boolean(process.env.NEXT_PUBLIC_HOSTED)
 
 export const isValidHost = (
   hostname: string,
   accessToken: string,
   forceProductionEnv?: boolean
 ) => {
-  const subdomain = makeSubdomain(hostname)
   const { slug, kind } = getInfoFromJwt(accessToken)
 
   const isInvalidChannel = kind !== "sales_channel"
-  const isInvalidSubdomain = subdomain !== slug || isInvalidChannel
-  if (
-    (isProduction(forceProductionEnv) && isHosted() && isInvalidSubdomain) ||
-    isInvalidChannel
-  ) {
+  if (isInvalidChannel) {
+    return false
+  }
+
+  // when app is not hosted by CL we can't rely on subdomain to match organization slug
+  // so we require to fill `NEXT_PUBLIC_SLUG` env
+  const subdomain = isCommerceLayerHosted()
+    ? makeSubdomain(hostname)
+    : process.env.NEXT_PUBLIC_SLUG
+
+  const isInvalidSubdomain = subdomain !== slug
+  if (isProduction(forceProductionEnv) && isInvalidSubdomain) {
     return false
   }
 
