36f29f78e2c9af7a8ba5bc8709022a17c59fbd7b
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2022-11-08 13:14:54 +0100

    fix: add new hook to propagate `keydown` (escape) and `blur` event to parent iframe, when embedded

--- components/Cart/index.tsx
+++ components/Cart/index.tsx
@@ -13,10 +13,12 @@ import { Summary } from "#components/Cart/Summary"
 import { PageHeader } from "#components/PageHeader"
 import { PageLayout } from "#components/PageLayout"
 import { useSettings } from "#components/SettingsProvider"
+import { useSendEmbeddedEvents } from "hooks/embedded"
 
 const Cart: FC = () => {
   const { settings } = useSettings()
   const { t } = useTranslation()
+  useSendEmbeddedEvents()
 
   if (!settings || !settings.isValid) {
     return null
