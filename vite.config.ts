5446b05641701c1f9512948fd1dee6beea296d40
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2022-11-09 12:16:28 +0100

    fix: add bundle visualizer when ANALYZE_BUNDLE is set as true in .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,50 @@
 import react from "@vitejs/plugin-react"
+import { visualizer } from "rollup-plugin-visualizer"
+import { loadEnv, PluginOption } from "vite"
 import { defineConfig } from "vitest/config"
 
 import path from "path"
 
+const makePlugins = (mode: string) => {
+  // Access .env variables in vite.config
+  Object.assign(process.env, loadEnv(mode, process.cwd(), ""))
+  const plugins: PluginOption[] = [
+    // Allow Vite to work with React
+    react(),
+    // Visualize and analyze bundle
+    process.env.ANALYZE_BUNDLE === "true" &&
+      visualizer({
+        filename: path.resolve(__dirname, "./build/stats.html"),
+        open: true,
+        title: "Bundle Stats",
+      }),
+  ].filter(Boolean)
+
+  return plugins
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  envPrefix: "PUBLIC_",
-  build: {
-    target: "esnext",
-    outDir: "build",
-  },
-  resolve: {
-    alias: {
-      "#styles": path.resolve(__dirname, "./src/styles"),
-      "#components": path.resolve(__dirname, "./src/components"),
-      "#assets": path.resolve(__dirname, "./src/assets"),
-      "#utils": path.resolve(__dirname, "./src/utils"),
-      "#specs": path.resolve(__dirname, "./specs"),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: makePlugins(mode),
+    envPrefix: "PUBLIC_",
+    build: {
+      target: "esnext",
+      outDir: "build",
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    include: ["src/**/*.{test,spec}.{ts,tsx}"],
-  },
+    resolve: {
+      alias: {
+        "#styles": path.resolve(__dirname, "./src/styles"),
+        "#components": path.resolve(__dirname, "./src/components"),
+        "#assets": path.resolve(__dirname, "./src/assets"),
+        "#utils": path.resolve(__dirname, "./src/utils"),
+        "#specs": path.resolve(__dirname, "./specs"),
+      },
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      include: ["src/**/*.{test,spec}.{ts,tsx}"],
+    },
+  }
 })
