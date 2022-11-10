3634428d3d2592a1db4b677afb294af667960048
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2022-11-10 11:52:36 +0100

    fix: load iframe-resizer script only when needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
+import { Helmet } from "react-helmet"
 import { Router, Route, Switch } from "wouter"
 
 import CartPage from "./pages/CartPage"
 import ErrorPage from "./pages/ErrorPage"
 
+import { isEmbedded } from "#utils/isEmbedded"
+
 function App(): JSX.Element {
   return (
-    <Router base={import.meta.env.PUBLIC_BASE_PATH}>
-      <Switch>
-        <Route path={"/404"}>
-          <ErrorPage />
-        </Route>
-        <Route path={"/:orderId"}>
-          <CartPage />
-        </Route>
-        <Route>
-          <ErrorPage />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <Helmet>
+        {isEmbedded() && (
+          <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.contentWindow.js"
+            data-test-id="iframe-resizer-script"
+            type="text/javascript"
+          />
+        )}
+      </Helmet>
+      <Router base={import.meta.env.PUBLIC_BASE_PATH}>
+        <Switch>
+          <Route path={"/404"}>
+            <ErrorPage />
+          </Route>
+          <Route path={"/:orderId"}>
+            <CartPage />
+          </Route>
+          <Route>
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   )
 }
 
