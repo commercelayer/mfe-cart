251ef9af7cab8fd4cd8bb830faea0c0c0d69efee
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-02-20 16:13:41 +0100

    fix: update AllowedLocaleKeys union

--- packages/cart/@typing/react-i18next.d.ts
+++ packages/cart/@typing/react-i18next.d.ts
@@ -2,7 +2,7 @@ import "react-i18next"
 import commonEn from "#assets/locales/en/common.json"
 
 declare module "react-i18next" {
-  export type AllowedLocaleKeys = "en" | "it"
+  export type AllowedLocaleKeys = "en" | "it" | "de"
 
   type AppResources = {
     common: typeof commonEn
