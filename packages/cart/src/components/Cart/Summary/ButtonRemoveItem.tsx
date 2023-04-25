092696e714db9da0781dcae1b9cde0c3e4b1c7ee
manju <manju@ezcontacts.com>
2023-04-25 11:45:47 +0530

    added cart ui chnages

--- packages/cart/src/components/Cart/Summary/ButtonRemoveItem.tsx
+++ packages/cart/src/components/Cart/Summary/ButtonRemoveItem.tsx
@@ -1,5 +1,5 @@
 import { LineItemRemoveLink } from "@commercelayer/react-components"
-import { FC } from "react"
+import { FC, useState } from "react"
 import { useTranslation } from "react-i18next"
 
 export const ButtonRemoveItem: FC = () => {
@@ -7,64 +7,129 @@ export const ButtonRemoveItem: FC = () => {
   const title = t("general.remove")
 
   return (
-    <LineItemRemoveLink>
-      {({ handleRemove }) => {
-        const onDelete = (event: any) => {
-          handleRemove(event)
-        }
-        return (
-          <a
-            onClick={(event) => onDelete(event)}
-            className="cursor-pointer hover:text-red-500"
-            title={title}
-            data-test-id="button-remove-item"
-          >
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 32 32"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M27 7H5"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M13 13V21"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M19 13V21"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M25 7V26C25 26.2652 24.8946 26.5196 24.7071 26.7071C24.5196 26.8946 24.2652 27 24 27H8C7.73478 27 7.48043 26.8946 7.29289 26.7071C7.10536 26.5196 7 26.2652 7 26V7"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M21 7V5C21 4.46957 20.7893 3.96086 20.4142 3.58579C20.0391 3.21071 19.5304 3 19 3H13C12.4696 3 11.9609 3.21071 11.5858 3.58579C11.2107 3.96086 11 4.46957 11 5V7"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </a>
-        )
-      }}
-    </LineItemRemoveLink>
+    <>
+      <LineItemRemoveLink>
+        {({ handleRemove }) => {
+          const [showModal, setShowModal] = useState(false)
+
+          const ConfirmModal = () => {
+            return (
+              <>
+                {" "}
+                {showModal ? (
+                  <>
+                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                      <div className="relative w-auto my-6 mx-auto max-w-sm">
+                        {/*content*/}
+                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                          {/*header*/}
+                          <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                            <h3 className="text-lg font-semibold">
+                             Shopping Cart
+                            </h3>
+                            <button
+                              className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                              onClick={() => setShowModal(false)}
+                            >
+                              <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                ×
+                              </span>
+                            </button>
+                          </div>
+                          {/*body*/}
+                          <div className="relative p-2 ml-4 mr-4 flex-auto">
+                            <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                              Are you sure you want to delete?.
+                            </p>
+                          </div>
+                          {/*footer*/}
+                          <div className="flex items-center justify-end p-2 border-t border-solid border-slate-200 rounded-b">
+                            <button
+                              className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                              type="button"
+                              onClick={(event) => onDelete(event)}
+                            >
+                              Yes
+                            </button>
+                            <button
+                              className="bg-emerald-500 text-black active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                              type="button"
+                              onClick={() => setShowModal(false)}
+                            >
+                              No
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                  </>
+                ) : null}
+              </>
+            )
+          }
+
+          const onDelete = (event: any) => {
+            handleRemove(event)
+          }
+
+          return (
+            <>
+              <a
+                 onClick={() => setShowModal(true)}
+                className="cursor-pointer hover:text-red-500"
+                title={title}
+                data-test-id="button-remove-item"
+              >
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M27 7H5"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M13 13V21"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M19 13V21"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M25 7V26C25 26.2652 24.8946 26.5196 24.7071 26.7071C24.5196 26.8946 24.2652 27 24 27H8C7.73478 27 7.48043 26.8946 7.29289 26.7071C7.10536 26.5196 7 26.2652 7 26V7"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M21 7V5C21 4.46957 20.7893 3.96086 20.4142 3.58579C20.0391 3.21071 19.5304 3 19 3H13C12.4696 3 11.9609 3.21071 11.5858 3.58579C11.2107 3.96086 11 4.46957 11 5V7"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </a>
+              <ConfirmModal />
+            </>
+          )
+        }}
+      </LineItemRemoveLink>
+    </>
   )
 }
--- packages/cart/src/components/Cart/Summary/index.tsx
+++ packages/cart/src/components/Cart/Summary/index.tsx
@@ -9,11 +9,9 @@ import {
 } from "@commercelayer/react-components"
 import { FC } from "react"
 import { useTranslation } from "react-i18next"
-
 import { ButtonRemoveItem } from "./ButtonRemoveItem"
 import { LineItemOptions } from "./LineItemOptions"
 import { QuantitySelector } from "./QuantitySelector"
-
 import { EmptyCartMessage } from "#components/atoms/EmptyCartMessage"
 import { useSettings } from "#components/SettingsProvider"
 import { LineItemsSkeleton } from "#components/Skeleton/LineItems"
@@ -24,10 +22,42 @@ type Props = {
 }
 
 export const Summary: FC<Props> = ({ listTypes }) => {
+
   const { t } = useTranslation()
   const { settings } = useSettings()
 
+  const goContinueShopping = () => {
+    window.location.href = "https://odoo.ezcontacts.com/"
+  }
 
+  const ContinueShopping = () => {
+    return (
+      <div onClick={goContinueShopping} className="flex items-center space-x-1 cursor-pointer">
+        <div>
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15 19.92L8.48 13.4C7.71 12.63 7.71 11.37 8.48 10.6L15 4.07996"
+              stroke="#292D32"
+              strokeWidth="1.5"
+              strokeMiterlimit="10"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+        <div
+          className="font-normal text-sm leading-5 text-gray-400 cursor-pointer">
+          {"Continue shopping"}
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
@@ -64,32 +94,35 @@ export const Summary: FC<Props> = ({ listTypes }) => {
                         <div className="font-normal text-sm leading-5 text-gray-700">
                           {attributeValue?.skuDisplayName}
                         </div>
-                       { attributeValue?.frame_size && <div className="pt-2">
-                          <div className="flex gap-1 text-sm">
-                            <div className="font-semibold text-xs leading-5 text-gray-700">
-                              {t("general.size")}:
-                            </div>
-                            <div className="font-normal text-xs leading-5 text-gray-400">
-                              {attributeValue?.frame_size}
-                            </div>
-                          </div>
-                        </div>}
-                        {attributeValue?.color && <div className="pt-2">
-                          <div className="flex gap-1 text-sm">
-                            <div className="font-semibold text-xs leading-5 text-gray-700">
-                              {t("general.color")}:
-                            </div>
-                            <div className="font-normal text-xs leading-5 text-gray-400">
-                              {attributeValue?.color}
+                        {attributeValue?.frame_size && (
+                          <div className="pt-2">
+                            <div className="flex gap-1 text-sm">
+                              <div className="font-semibold text-xs leading-5 text-gray-700">
+                                {t("general.size")}:
+                              </div>
+                              <div className="font-normal text-xs leading-5 text-gray-400">
+                                {attributeValue?.frame_size}
+                              </div>
                             </div>
                           </div>
-                        </div>}
+                        )}
+                        {attributeValue?.color && (
+                          <div className="pt-2">
+                            <div className="flex gap-1 text-sm">
+                              <div className="font-semibold text-xs leading-5 text-gray-700">
+                                {t("general.color")}:
+                              </div>
+                              <div className="font-normal text-xs leading-5 text-gray-400">
+                                {attributeValue?.color}
+                              </div>
+                            </div>
+                          </div>
+                        )}
                       </div>
                     )
                   }}
                 </LineItemField>
                 <ButtonRemoveItem />
-
               </div>
 
               <LineItemOptions />
@@ -119,6 +152,9 @@ export const Summary: FC<Props> = ({ listTypes }) => {
           </div>
         </LineItem>
       ))}
+      <div>
+        <ContinueShopping />
+      </div>
 
       {/* Empty cart */}
       <LineItemsEmpty>
