1237ff025fb0d347b326f1f5e9f4284cabb0a41a
manju <manju@ezcontacts.com>
2023-05-02 20:43:16 +0530

    fixed payment token issue

--- packages/cart/src/components/Cart/Totals/ButtonCheckout.tsx
+++ packages/cart/src/components/Cart/Totals/ButtonCheckout.tsx
@@ -16,17 +16,55 @@ export const ButtonCheckout: FC = () => {
     return null
   }
 
-  const onProceedCheckout = () => {
-    window.open(
-      `https://ezcontacts-stage-checkout.netlify.app/${settings.orderId }?accessToken=${settings.accessToken}`
-    );
+  const onProceedCheckout = async () => {
+    if (settings.orderId) {
+      let paymentToken = await getPaymentToken(settings.orderId)
+      window.open(
+        `https://ezcontacts-stage-checkout.netlify.app/${settings.orderId}?accessToken=${settings.accessToken}&paymentToken=${paymentToken}`,
+        "_self"
+      )
+    }
+  }
+
+  const getPaymentToken = (orderId: any) => {
+    if (orderId) {
+      const requestBody = {
+        data: {
+          order: {
+            id: orderId,
+          },
+          customer: {
+            email: "ezdev.mahesh@gmail.com",
+          },
+        },
+      }
+      return fetch(
+        `https://odoo.ezcontacts.com/cl/order/payment/v1/payment-token`,
+        {
+          headers: {
+            Accept: "application/json",
+          },
+          method: "POST",
+          body: JSON.stringify(requestBody),
+        }
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          const res = result?.data?.payment_source_token
+          return res
+        })
+        .catch((error) => {
+          console.error("Error:", error)
+        })
+    }
   }
 
   return (
     <>
       <LineItemsCount>
         {({ quantity }) =>
-          quantity ? (  <div
+          quantity ? (
+            <div
               className="button-checkout text-center text-white py-2 px-4 rounded"
               onClick={onProceedCheckout}
             >
