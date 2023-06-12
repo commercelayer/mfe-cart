40869160135fd0097b21b04e0c4adc13b8f3976b
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-06-12 11:46:45 +0200

    feat: add `expressPayments`

--- packages/cart/src/components/Cart/Totals/ButtonCheckout.tsx
+++ packages/cart/src/components/Cart/Totals/ButtonCheckout.tsx
@@ -1,4 +1,11 @@
-import { CheckoutLink, LineItemsCount } from "@commercelayer/react-components"
+import {
+  CheckoutLink,
+  LineItemsCount,
+  PaymentMethod,
+  PaymentMethodsContainer,
+  PaymentSource,
+  useOrderContainer,
+} from "@commercelayer/react-components"
 import { FC } from "react"
 import { useTranslation } from "react-i18next"
 
@@ -8,9 +15,17 @@ import { isEmbedded } from "#utils/isEmbedded"
 export const ButtonCheckout: FC = () => {
   const { t } = useTranslation()
   const label = t("general.gotToCheckoutCta")
+  const { order } = useOrderContainer()
 
   return (
     <>
+      <div key={order?.total_amount_cents}>
+        <PaymentMethodsContainer>
+          <PaymentMethod expressPayments className="mb-4">
+            <PaymentSource />
+          </PaymentMethod>
+        </PaymentMethodsContainer>
+      </div>
       <LineItemsCount>
         {({ quantity }) =>
           quantity ? (
