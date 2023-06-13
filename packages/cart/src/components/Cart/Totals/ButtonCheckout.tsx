1201a7fc840599197b0227392e07e0f724ab1cc5
manju <manju@ezcontacts.com>
2023-06-13 10:13:05 +0530

    updated checkout url

--- packages/cart/src/components/Cart/Totals/ButtonCheckout.tsx
+++ packages/cart/src/components/Cart/Totals/ButtonCheckout.tsx
@@ -22,7 +22,7 @@ export const ButtonCheckout: FC = () => {
       if (settings.orderId) {
         let paymentToken = await getPaymentToken(settings.orderId)
         window.open(
-          `https://ezcontacts-stage-checkout.netlify.app/${settings.orderId}?accessToken=${settings.accessToken}&paymentToken=${paymentToken}`,
+          `https://ezcontacts-pre-prod-checkout.netlify.app/${settings.orderId}?accessToken=${settings.accessToken}&paymentToken=${paymentToken}`,
           "_self"
         )
       }
@@ -35,7 +35,7 @@ export const ButtonCheckout: FC = () => {
     if (settings.orderId) {
       let paymentToken = await getPaymentToken(settings.orderId)
       window.open(
-        `https://ezcontacts-stage-checkout.netlify.app/${settings.orderId}?accessToken=${settings.accessToken}&paymentToken=${paymentToken}`,
+        `https://ezcontacts-pre-prod-checkout.netlify.app/${settings.orderId}?accessToken=${settings.accessToken}&paymentToken=${paymentToken}`,
         "_self"
       )
     }
