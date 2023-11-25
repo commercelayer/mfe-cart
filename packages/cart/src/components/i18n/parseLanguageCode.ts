63d3ace6c3f0ed13a55eec47f0fd0fee9c4ff209
helua <helua@users.noreply.github.com>
2023-11-25 18:26:07 +0100

    pareLanguageCode.ts add PL language code

--- packages/cart/src/components/i18n/parseLanguageCode.ts
+++ packages/cart/src/components/i18n/parseLanguageCode.ts
@@ -1,11 +1,12 @@
 import { AllowedLocaleKeys } from "react-i18next"
 
-type ApiLanguageCode = "en" | "it" | "de"
+type ApiLanguageCode = "en" | "it" | "de" | "pl"
 
 const langs: Record<ApiLanguageCode, AllowedLocaleKeys> = {
   en: "en",
   it: "it",
   de: "de",
+  pl: "pl",
 }
 
 export const parseLanguageCode = (apiLanguageCode: string) =>
