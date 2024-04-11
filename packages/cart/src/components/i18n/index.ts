1faae435393c9ef471330bb9cd32610c49927ee4
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2024-04-11 14:34:54 +0200

    fix: add Hungarian locale

--- packages/cart/src/components/i18n/index.ts
+++ packages/cart/src/components/i18n/index.ts
@@ -7,6 +7,7 @@ import {
 
 import commonDe from "#assets/locales/de/common.json"
 import commonEn from "#assets/locales/en/common.json"
+import commonHu from "#assets/locales/hu/common.json"
 import commonIt from "#assets/locales/it/common.json"
 import commonPl from "#assets/locales/pl/common.json"
 
@@ -22,6 +23,9 @@ const resources: Record<AllowedLocaleKeys, AppResources> = {
   },
   pl: {
     common: commonPl,
+  },
+  hu: {
+    common: commonHu,
   },
 }
 
--- packages/cart/src/components/i18n/parseLanguageCode.ts
+++ packages/cart/src/components/i18n/parseLanguageCode.ts
@@ -1,12 +1,13 @@
 import { AllowedLocaleKeys } from "react-i18next"
 
-type ApiLanguageCode = "en" | "it" | "de" | "pl"
+type ApiLanguageCode = "en" | "it" | "de" | "pl" | "hu"
 
 const langs: Record<ApiLanguageCode, AllowedLocaleKeys> = {
   en: "en",
   it: "it",
   de: "de",
   pl: "pl",
+  hu: "hu",
 }
 
 export const parseLanguageCode = (apiLanguageCode: string) =>
