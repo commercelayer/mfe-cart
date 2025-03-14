983f16f1aaf326931ca21c6d2cd13c7940f02ed4
Matteo Alessani <matteo@commercelayer.io>
2025-03-14 16:44:02 +0100

    fix: refine buttons and use checkout url when using express payments

--- packages/cart/src/components/Cart/Totals/ButtonCheckout.tsx
+++ packages/cart/src/components/Cart/Totals/ButtonCheckout.tsx
@@ -19,22 +19,6 @@ export const ButtonCheckout: FC = () => {
 
   return (
     <>
-      <div key={order?.total_amount_cents}>
-        <PaymentMethodsContainer>
-          <PaymentMethod
-            expressPayments
-            className="mb-4"
-            loader={<div />}
-            hide={({ payment_source_type }) => {
-              // only show stripe payments
-              return payment_source_type === "stripe_payments"
-            }}
-          >
-            <PaymentSource loader={<div />} />
-          </PaymentMethod>
-        </PaymentMethodsContainer>
-      </div>
-
       <Errors
         resource="line_items"
         className="block text-xs text-red-400 mb-4"
@@ -56,7 +40,7 @@ export const ButtonCheckout: FC = () => {
               data-test-id="button-checkout"
               aria-disabled="false"
               className={
-                "button-base bg-primary text-contrast block rounded-md py-3 px-3 !text-xs md:!text-base"
+                "button-base bg-primary text-contrast block rounded-md py-3 px-3 text-base"
               }
               label={label}
               target={isEmbedded() ? "_top" : undefined}
@@ -66,6 +50,22 @@ export const ButtonCheckout: FC = () => {
           )
         }
       </LineItemsCount>
+
+      <div key={order?.total_amount_cents}>
+        <PaymentMethodsContainer>
+          <PaymentMethod
+            expressPayments
+            className="mt-4"
+            loader={<div />}
+            hide={({ payment_source_type }) => {
+              // only show stripe payments
+              return payment_source_type === "stripe_payments"
+            }}
+          >
+            <PaymentSource loader={<div />} />
+          </PaymentMethod>
+        </PaymentMethodsContainer>
+      </div>
     </>
   )
 }
