3091b0a9a8cee5cbe4a4f101189ddef165c427a3
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2025-05-08 21:38:11 +0200

    chore: update dependencies

--- packages/cart/src/utils/frequency.ts
+++ packages/cart/src/utils/frequency.ts
@@ -1,11 +1,11 @@
-import { parseExpression } from "cron-parser"
+import CronExpressionParser from "cron-parser"
 
 export function isValidCronExpression(
   // biome-ignore lint/suspicious/noExplicitAny: we receive any from react components
   cronExpression: any,
 ): cronExpression is string {
   try {
-    parseExpression(cronExpression)
+    CronExpressionParser.parse(cronExpression)
     return true
   } catch {
     return false
